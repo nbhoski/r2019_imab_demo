b7c033e23b7ae16bf39d02a5634812849defeef7
workshop-mw <workshop-mw@BGL-WSLAPTOP-19.training.mathworks.com>
2019-08-19 20:47:33 +0530

    changed file for demo.

--- tests/testSquareRoot_parameterized.m
+++ tests/testSquareRoot_parameterized.m
@@ -1,8 +1,8 @@
 classdef testSquareRoot_parameterized < matlab.unittest.TestCase
     
     properties (TestParameter)
-        inputs = {4, 16, 81, 121};
-        expected_outputs = {2, 4, 9 , 11};
+        inputs = {4, 16, 81, 121, 49};
+        expected_outputs = {2, 4, 9 , 11, 7};
     end
     
     methods (TestClassSetup)
