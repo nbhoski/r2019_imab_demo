1170bba0fe4d92f604f6e88655804ce00fab2808
nbhoski <nikhil.bhoski@mathworks.in>
2020-02-14 14:02:23 +0530

    kl

--- tests/testSquareRoot_basic.m
+++ tests/testSquareRoot_basic.m
@@ -10,7 +10,7 @@ classdef testSquareRoot_basic < matlab.unittest.TestCase
     methods (Test)
         function testValues(testCase)
             testCase.verifyEqual(squareRoot(16),4);
-            %testCase.verifyEqual(squareRoot(65536),256);            
+            testCase.verifyEqual(squareRoot(81),9);            
         end
         
     end
