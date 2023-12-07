bf5bba629710c094ab1ab2de199558a75caf4a88
Nikhil Bhoski <47204011+nbhoski@users.noreply.github.com>
2023-12-07 14:50:03 +0530

    Add files via upload

---
+++ buildfile.m
@@ -0,0 +1,38 @@
+function plan = buildfile
+% Copyright 2023 The MathWorks, Inc.
+ 
+import matlab.buildtool.tasks.*
+import matlab.buildtool.Task;
+ 
+plan = buildplan(localfunctions);
+
+plan("check") = Task;
+plan("check").Actions = @checkTask;
+plan("check").Description = "Checks Description";
+plan("test") = Task;
+plan("test").Actions = @testTask;
+plan("test").Description = "tests Dscription";
+plan("test").Dependencies = ["check","show"];
+plan("show") = Task;
+plan("show").Actions = @showTask;
+
+%plan("dsplay")
+
+plan("clean") = CleanTask();
+plan.DefaultTasks = ["test"];
+
+function checkTask(~)
+ % Identify code issues
+disp("Hello Check");
+end
+function showTask(~)
+ % Identify code issues
+disp("Hello Show");
+end
+
+function testTask(~)
+ % Identify code issues
+disp("Hello test");
+end
+     
+end
